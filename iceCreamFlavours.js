d2cddebc9044036730e472ea69fd4bdfa6b6a165
Gabriel Cadiz <gabecadiz@hotmail.com>
2019-02-02 19:01:20 +0000

    LHL Arrays Ice Cream Assignment

---
+++ iceCreamFlavours.js
@@ -0,0 +1,14 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//step 1 add "root beer" to the array without modifying the original array
+iceCreamFlavours.push("root beer");
+
+//step 2 console.log the first flavour of the array
+console.log(iceCreamFlavours[0]);
+
+//step 3 console.log the last flavour of the array
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//step 4 add a line of code that console.logs the number of flavours in the array
+console.log(iceCreamFlavours.length);
